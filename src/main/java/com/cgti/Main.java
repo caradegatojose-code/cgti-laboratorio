74b863f6f8107026f34667773503c8377baadd5e
jose <caradegatojose@gmail.com>
2026-05-27 20:46:41 -0600

    proyecto base .2

---
+++ src/main/java/com/cgti/Main.java
@@ -0,0 +1,24 @@
+package com.cgti;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Bienvenido al sistema CGTI");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("CGTI - Control de Laboratorios");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
